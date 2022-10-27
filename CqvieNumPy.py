939192164343e2ddbc98a3949afe85902b23ad3f
jch9497 <jchlovedxy@outlook.com>
2022-10-27 12:11:10 +0800

    py

---
+++ CqvieNumPy.py
@@ -0,0 +1,7 @@
+print("计算x只青蛙，y只兔子，z只鸡有多少只腿")
+x = int(input("请输入多少只青蛙\n"))
+y = int(input("请输入多少只兔子\n"))
+z = int(input("请输入多少只鸡\n"))
+num = 4*x+4*y+2*z
+print("青蛙%d只，兔子%d只，鸡%d只，一共有%d只腿\n" % (x, y, z, num))
+input("\n")
